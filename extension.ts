62d443a2bb31fd85e55e3604b69a57e07ddcedc0
L <lemo1234@gmail.com>
2016-08-20 17:21:49 -0700

    Fix tabs -> spaces in the new changes

--- extension.ts
+++ extension.ts
@@ -156,7 +156,7 @@ export function activate(context: vscode.ExtensionContext) {
             }
         }
 
-		public static normalize(uri: string): string {
+        public static normalize(uri: string): string {
             // a simple workaround for what appears to be a vscode.Uri bug
             // (inconsistent fsPath values for the same document, ex. ///foo/x.cpp and /foo/x.cpp)
             return uri.replace('///', '/');
