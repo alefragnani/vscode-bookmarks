a6e4a30d8259641991d2e7f75f498cdffb9a9699
Alessandro Fragnani <alefragnani@hotmail.com>
2021-03-03 01:09:40 -0300

    Update Tabnine URL

--- src/whats-new/contentProvider.ts
+++ src/whats-new/contentProvider.ts
@@ -357,7 +357,7 @@ export class BookmarksSponsorProvider implements SponsorProvider {
         sponsors.push(sponsorCodeStream);
         const sponsorTabnine: Sponsor = <Sponsor>{
             title: "Learn more about Tabnine",
-            link: "https://bit.ly/2LZsrQ9",
+            link: "http://wd5a.2.vu/Bookmarks",
             image: {
                 dark: "https://github.com/alefragnani/oss-resources/raw/master/images/sponsors/tabnine-hi-res.png",
                 light: "https://github.com/alefragnani/oss-resources/raw/master/images/sponsors/tabnine-hi-res.png"
